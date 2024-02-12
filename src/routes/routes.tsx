5c081630811e4fc5f41315694d4c67514d9ca905
jessica-sobreira <j.jecristina@gmail.com>
2024-02-12 18:02:40 -0300

    login e signup

---
+++ src/routes/routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Login />,
+    },
+    {
+        path: '/criar-usuario',
+        element: <SignUp />,
+    },
+
+])
+
