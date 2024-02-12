62a99515b6bf55b49031a26525a1bdc70384b838
jessica-sobreira <j.jecristina@gmail.com>
2024-02-12 17:42:01 -0300

    configuração redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import { Provider } from "react-redux";
+import { store } from "./config/store";
 
 function App() {
-
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+        <Provider store={store}>
+            <div>hello</div>
+        </Provider>
+            
+        </>
+    );
 }
 
-export default App
+export default App;
